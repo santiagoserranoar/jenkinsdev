795033917cd3ffef43bd34fd73c38a96206647df
root <root@jenkinsdev.sanatorioallende.com>
2021-03-09 22:29:24 -0300

    with sudo

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 sudo mount -t cifs -o username=vagrant,password='K3n3d121', //192.168.32.11/ServiciosInfra /mnt/backendev
 sudo cd /mnt/backendev
 sudo cd DEV/
-sudo for i in $(ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do rm -rf /mnt/backendev/DEV/$i; done
+for i in $(sudo ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do sudo rm -rf /mnt/backendev/DEV/$i; done
 sudo cp -R /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/* .
 sudo cd /
 sudo umount /mnt/backendev
