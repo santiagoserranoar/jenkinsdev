4b68e52db64b5d493967c8b578d205a55e7ee708
root <root@jenkinsdev.sanatorioallende.com>
2021-03-09 22:04:29 -0300

    new

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
-mount -t cifs -o username=sserrano,password='Cdc6to616##',workgroup=ALLENDE //192.168.32.11/ServiciosInfra /mnt/backendev
+mount -t cifs -o username=vagrant,password='K3n3d121', //192.168.32.11/ServiciosInfra /mnt/backendev
 cd /mnt/backendev
-rm -rf DEV
-mkdir DEV
 cd DEV/
+for i in $(ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do rm -rf /mnt/backendev/DEV/$i; done
 cp -R /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/* .
+cd /
 umount /mnt/backendev
