22f15e626a2db996bbe11b3049fe45bcd8f43474
root <root@jenkinsdev.sanatorioallende.com>
2021-03-09 22:26:54 -0300

    with sudo

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 sudo mount -t cifs -o username=vagrant,password='K3n3d121', //192.168.32.11/ServiciosInfra /mnt/backendev
-sudo /mnt/backendev
+sudo cd /mnt/backendev
 sudo cd DEV/
 sudo for i in $(ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do rm -rf /mnt/backendev/DEV/$i; done
 sudo cp -R /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/* .
