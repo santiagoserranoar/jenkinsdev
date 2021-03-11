a9f589ae788f3f5433341694d1765ebb4fce7c55
santiagoserranoar <santiagoserrano.ar@gmail.com>
2021-03-10 21:53:56 -0300

    new

--- apptest/DeployAppTest.sh
+++ apptest/DeployAppTest.sh
@@ -1,4 +1,4 @@
-sudo mount -t cifs -o username=vagrant,password='K3n3d121', //192.168.32.11/ServiciosSA /mnt/backendtest
-for i in $(sudo ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do sudo rm -rf /mnt/backentest/DEV/$i; done
-sudo cp -R /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/* /mnt/backendev/DEV/
-sudo umount /mnt/backendev
+sudo mount -t cifs -o username=vagrant,password='K3n3d121', //192.168.32.40/ServiciosSA /mnt/backendtest
+for i in $(sudo ls -la /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/); do sudo rm -rf /mnt/backendtest/TEST/$i; done
+sudo cp -R /var/lib/jenkins/workspace/BackendDev/Deploy/Publish/Development/* /mnt/backendtest/TEST/
+sudo umount /mnt/backendtest
